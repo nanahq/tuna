4ca7783c2ae5a93a33b1200c4ec84df0fe08dba8
siradji <siradjiawoual@gmail.com>
2024-03-04 09:14:08 +0100

    chore: updated version code

--- mobile-app/hooks/useExpoNotification.tsx
+++ mobile-app/hooks/useExpoNotification.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button } from 'react-native';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import Constants from "expo-constants";
