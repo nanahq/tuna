2815bd23756c90eedd4dc0404dd4cc4a0890d1cd
siradji <siradjiawoual@gmail.com>
2024-09-19 15:00:35 +0100

    feature: added datadog

---
+++ mobile-app/screens/Wrapper.tsx
@@ -0,0 +1,37 @@
+import {
+    BatchSize,
+    DatadogProvider,
+    DatadogProviderConfiguration,
+    SdkVerbosity,
+    UploadFrequency,
+} from "@datadog/mobile-react-native";
+import {PropsWithChildren} from "react";
+
+
+const config = new DatadogProviderConfiguration(
+    "pubd7316220d1b7c35e4204285ee5d7b31f",
+    process.env.EXPO_NODE_ENV,
+    "f0a5596b-63b6-44e6-a554-12902be801c2",
+    true,
+    true,
+    true
+)
+config.site = "US1"
+config.longTaskThresholdMs = 100
+config.nativeCrashReportEnabled = true
+config.sampleRate = 100
+
+if (__DEV__) {
+    config.uploadFrequency = UploadFrequency.FREQUENT
+    config.batchSize = BatchSize.SMALL
+    config.verbosity = SdkVerbosity.DEBUG
+}
+
+export  function Wrapper({children}: PropsWithChildren<{}>) {
+    return (
+        <DatadogProvider configuration={config}>
+            {children}
+        </DatadogProvider>
+    );
+}
+
